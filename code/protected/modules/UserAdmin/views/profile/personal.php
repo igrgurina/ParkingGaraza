8ce9064076b13ef8702912d4a6e45daaae062612
Alvareto <ivan.grgurina@gmail.com>
2014-11-01 21:25:55 +0100

    git me zajebo

--- code/protected/modules/UserAdmin/views/profile/personal.php
+++
@@ -1,5 +0,0 @@
-<h4 class='centered'>You can change your password (or other allowed fields here)</h4>
-
-<div class='alert alert-info centered'>
-        User login: <?php echo $user->login; ?>
-</div>
