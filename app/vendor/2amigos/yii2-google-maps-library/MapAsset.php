3533dcbf9d935a915af2592d7c3fbae65f87f6d0
Alvareto <ivan.grgurina@gmail.com>
2014-11-03 15:24:02 +0100

    yii2 framework - this fixes #10

---
+++ app/vendor/2amigos/yii2-google-maps-library/MapAsset.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * @copyright Copyright (c) 2014 2amigOS! Consulting Group LLC
+ * @link http://2amigos.us
+ * @license http://www.opensource.org/licenses/bsd-license.php New BSD License
+ */
+namespace dosamigos\google\maps;
+
+use Yii;
+use yii\web\AssetBundle;
+
+/**
+ * MapAsset
+ *
+ * Registers the google maps api
+ *
+ * @author Antonio Ramirez <amigo.cobos@gmail.com>
+ * @link http://www.ramirezcobos.com/
+ * @link http://www.2amigos.us/
+ * @package dosamigos\google\maps
+ */
+class MapAsset extends AssetBundle
+{
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        // To configure please, add `googleMapsApiKey` parameter to your application configuration
+        // file with the value of your API key. To get yours, please visit https://code.google.com/apis/console/.
+        $key = @Yii::$app->params['googleMapsApiKey'];
+
+        $this->js[] = 'https://maps.googleapis.com/maps/api/js?key=' . $key;
+    }
+} 
--- app/views/parking/_form.php
+++ app/views/parking/_form.php
@@ -2,6 +2,7 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use kolyunya\yii2\widgets\MapInputWidget;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\Parking */
