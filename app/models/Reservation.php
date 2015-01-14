e556c0b83cf623d5dbb12f132e1b41ff196aad5e
De117 <maldini.mokosica@gmail.com>
2015-01-14 18:59:09 +0100

    dodano jos par komentara u Reservation model

--- app/models/Reservation.php
+++ app/models/Reservation.php
@@ -10,14 +10,15 @@ use Yii, DateTime, DateInterval;
  * @property integer $user_id
  * @property string $type
  * @property integer $parking_id
- * @property string $start
- * @property string $end
- * @property string $duration
- * @property string $period
+ * @property string $start			// timestamp pocetka prvog termina
+ * @property string $end			// timestamp kraja prvog termina
+ * @property string $duration		// trajanje rezervacije u danima (=1 za instant rezervacije)
+ * @property string $period			// period ponavljanja u danima (null za instant i permanent rezervacije)
  * @property bool $active
  *
  * @property User $user
  * @property Parking $parking
+ * 
  */
 class Reservation extends \yii\db\ActiveRecord
 {
@@ -89,64 +90,116 @@ class Reservation extends \yii\db\ActiveRecord
 
     public function isPossible()
     {
-        $start = new DateTime($this->start);
-        $end = new DateTime($this->end);
+        $start = new DateTime($this->start);		// vrijeme pocetka prvog termina
+        $end = new DateTime($this->end);			// vrijeme kraja prvog termina
 
         if ($this->type == 'instant') {
-
-            $count = $this->activeReservationsAt($start, $end);
-
+			
+			// u slucaju jednokratne rezervacije, imamo samo jedan termin
+			
+            $count = $this->activeReservationsAt($start, $end);		// i prebrojimo kolizije u njemu
+			
             return (($count >= $this->parking->number_of_parking_spots) ? (false) : (true));
 
-        } elseif ($this->type=='recurring' or $this->type=='permanent'){
-
-            for ($i = $start; $i <= $end; $i->add(new DateInterval( 'P1D'/* 'P' . $this->period . 'D' */))) {
-                $j = $i;
-                if($end == $i) return false;
-
-                if ($this->activeReservationsAt($i, $j->add(new DateInterval('PT' . $this->duration . 'H')))
-                        >= $this->parking->number_of_parking_spots)
+        } elseif ($this->type=='recurring'){
+		
+			// u slucaju ponavljajuce rezervacije, imamo po jedan termin svaki $period-ti dan, i tako $duration dana
+			
+            for ($i = 1; $i <= $this->duration; $i += $this->period) {
+                
+                if ($this->activeReservationsAt($start, $end) >= $this->parking->number_of_parking_spots)
                     return false;
-                // if ( activeReservationsAt(start, start+duration) >= totalSpotNum) return false;
-                // ako prezivi sve iteracije, znaci da nema kolizije i vraca true
+				// pa provjeravamo broj kolizija u svakom od tih termina
+				
+				$start->add(new DateInterval('P' . $this->period . 'D' ));		// dodaje $period dana
+				$end->add(new DateInterval('P' . $this->period . 'D' ));		// dodaje $period dana
+				// i na kraju svakog prolaska kroz petlju se pomicemo na sljedeci termin
 
             }
+			// ako prezivi sve iteracije, znaci da nema previse kolizija niti u jednom terminu, i vraca true
             return true;
-        }
+			
+        } elseif ($this->type=='permanent') {
+			
+			// u slucaju ponavljajuce rezervacije, imamo po jedan termin svaki dan, i tako unedogled
+			// Q: koliko unaprijed trebamo provjeravati kolizije, odnosno koliko unaprijed se smije rezervirati?
+			// ovdje pretpostavljam 366 dana
+			
+			for ($i = 1; $i <= 366; $i++) {
+				
+				// sama petlja je prakticki ista kao i kod ponavljajuce rezervacije
+				
+				if ($this->activeReservationsAt($start,$end) >= $this->parking->number_of_parking_spots)
+					return false;
+				
+				$start->add(new DateInterval('P1D'));
+				$end->add(new DateInterval('P1D'));
+			}
+			return true;
+			
+		}
     }
 
    /*
     * Counts collisions in certain day
     */
-
-
     private function activeReservationsAt ($start, $end) {
 
-        $count = 0;
-        foreach($this->parking->reservations as $res) {
+        $count = 0;		// inicijalizacija brojaca
+        foreach($this->parking->reservations as $res) {			// za svaku rezervaciju promatranog parkinga
 
-            if (true /* $res->active  - NEMA GA U BAZI!*/) {
-                $cmpstart = new DateTime($res->start);
+            if (true /* trebalo bi biti "$res->active", ali nema ga u bazi */) {	// ako nije neaktivna
+                
+				// $start i $end pripadaju traženom terminu ulazne rezervacije
+				// $cmpstart i $cmpend pripadaju terminu rezervacije u ovoj iteraciji petlje (u daljnjim komentarima cmprezervacija)
+					// za kojeg gledamo je li u koliziji sa ulaznim terminom
+				$cmpstart = new DateTime($res->start);
                 $cmpend = new DateTime($res->end);
 
-                if (($cmpend > $start) && ($cmpstart < $end)) {
-
-                    if ($res->type == 'instant')
-                        $count++;
-                    else {
-                        for ($i = $cmpstart; $i < $cmpend;) {
-                            if (($i < $end) and ($i->add(new DateInterval('PT' . $this->duration . 'H')) > $start)) {
-                                $count++;
-                                break;
-                            }
-                            $i->add(new DateInterval('P1D'));   // adds a day
-                        }
-                    }
-                }
+                if ($res->type == 'instant') {
+					
+					// ukoliko je cmprezervacija jednokratna, ima samo jedan termin (za koji provjeravamo kolizije)
+					
+					if (($cmpend > $start) && ($cmpstart < $end))
+						$count++;
+					
+				} elseif ($res->type == 'recurring') {
+					
+					// ukoliko je cmprezervacija ponavljajuca, ima cijeli set termina (svaki $period-ti dan, i tako $duration dana)
+					// i za svaki od tih termina gledamo radi li koliziju sa ulaznim terminom
+					
+					for ($i = 1; $i <= $res->duration; $i += $res->period) {
+						
+						if (($cmpend > $start) && ($cmpstart < $end)) {
+							$count++;
+							break;
+							// ukoliko nađemo da radi koliziju s ulaznim terminom, 
+							// ne trebamo traziti dalje, vec idemo na sljedecu cmprezervaiciju
+						}
+						$cmpstart->add(new DateInterval('P' . $res->period . 'D' ));	// dodaje $period dana
+						$cmpend->add(new DateInterval('P' . $res->period . 'D' ));		// dodaje $period dana
+					}
+					
+				} elseif ($res->type == 'permanent') {
+					
+					// ukoliko je cmprezervacija trajna, situacija je slicna kao kod ponavljajuce
+					
+					$temp = $start;
+					$temp->add(new DateInterval('P' . $this->duration . 'D' );
+					// zapravo, treba mi while($cmpstart <= $start+$duration), ali ne nalazim prikladnu DateTime funkciju za takvo sto
+					
+					while ($cmpstart <= $temp) {
+						
+						if (($cmpend > $start) && ($cmpstart < $end)) {
+							$count++;
+							break;
+						}
+						$cmpstart->add(new DateInterval('P' . $res->period . 'D' ));	// dodaje $period dana
+						$cmpend->add(new DateInterval('P' . $res->period . 'D' ));		// dodaje $period dana
+					}
+				}
             }
         }
         return $count;
     }
 }
-
-
